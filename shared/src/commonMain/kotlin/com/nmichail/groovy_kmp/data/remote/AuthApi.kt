5799fdfdef858593a95847741ca0c49ac559896b
Mikhail Nikolaev <nmichail154@mail.ru>
2025-06-21 19:59:55 +0700

    Login feature works

--- shared/src/commonMain/kotlin/com/nmichail/groovy_kmp/data/remote/AuthApi.kt
+++ shared/src/commonMain/kotlin/com/nmichail/groovy_kmp/data/remote/AuthApi.kt
@@ -10,39 +10,31 @@ import com.nmichail.groovy_kmp.domain.models.AuthResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.withContext
-import android.util.Log
 import kotlinx.serialization.json.Json
 
 class AuthApi(private val client: HttpClient) {
     suspend fun login(email: String, password: String): AuthResponse {
         return withContext(Dispatchers.IO) {
             try {
-                Log.d("AuthApi", "Отправляем запрос на логин")
-                Log.d("AuthApi", "Email: $email, Password: $password")
-                
+
                 val request = AuthRequest(email, password, email)
-                Log.d("AuthApi", "Request body: $request")
-                
-                val json = Json { 
-                    prettyPrint = true 
-                    isLenient = true 
-                    ignoreUnknownKeys = true 
+
+                val json = Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
                 }
                 val jsonString = json.encodeToString(AuthRequest.serializer(), request)
-                Log.d("AuthApi", "Serialized JSON: $jsonString")
-                
+
                 val response: HttpResponse = client.post("http://127.0.0.1:8080/auth/login") {
                     contentType(ContentType.Application.Json)
                     setBody(jsonString)
                 }
-                
-                Log.d("AuthApi", "Response status: ${response.status}")
+
                 val responseBody = response.body<AuthResponse>()
-                Log.d("AuthApi", "Response body: $responseBody")
-                
+
                 responseBody
             } catch (e: Exception) {
-                Log.e("AuthApi", "Ошибка при логине: ${e.message}")
                 e.printStackTrace()
                 throw e
             }
