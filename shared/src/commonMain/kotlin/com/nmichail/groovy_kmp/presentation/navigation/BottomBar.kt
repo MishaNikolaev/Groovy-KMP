383939f7a99552ca4022a19d667e14542902de86
Mikhail Nikolaev <nmichail154@mail.ru>
2025-06-25 17:25:53 +0700

    BottomBar.kt fix

--- shared/src/commonMain/kotlin/com/nmichail/groovy_kmp/presentation/navigation/BottomBar.kt
+++ shared/src/commonMain/kotlin/com/nmichail/groovy_kmp/presentation/navigation/BottomBar.kt
@@ -2,6 +2,7 @@ package com.nmichail.groovy_kmp.presentation.navigation
 
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Explore
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
@@ -12,6 +13,7 @@ import androidx.compose.material.icons.filled.HomeMax
 import androidx.compose.material.icons.filled.MusicNote
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Explore
 import androidx.compose.material.icons.outlined.HomeMax
 import androidx.compose.material.icons.outlined.MusicNote
 import androidx.compose.material3.Icon
@@ -22,7 +24,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import groovy_kmp.shared.generated.resources.Res
+import groovy_kmp.shared.generated.resources.music_notes_simple
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun BottomBar(
     currentRoute: String?,
@@ -37,15 +44,23 @@ fun BottomBar(
                 selected = selected,
                 onClick = { onNavigate(item.screen) },
                 icon = {
-                    Icon(
-                        imageVector = if (selected) item.selectedIcon else item.unselectedIcon,
-                        contentDescription = item.screen.route,
-                        modifier = if (item.screen == Screen.MainSection.Home) Modifier.size(32.dp) else Modifier
-                    )
+                    if (item.screen == Screen.MainSection.Home) {
+                        Icon(
+                            painter = painterResource(Res.drawable.music_notes_simple),
+                            contentDescription = item.screen.route,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    } else {
+                        Icon(
+                            imageVector = if (selected) item.selectedIcon else item.unselectedIcon,
+                            contentDescription = item.screen.route,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
                 },
                 colors = NavigationBarItemDefaults.colors(
                     selectedIconColor = Color.Black,
-                    unselectedIconColor = Color.Black,
+                    unselectedIconColor = Color.Gray,
                     indicatorColor = Color.White
                 )
             )
@@ -62,13 +77,13 @@ private data class BottomNavItem(
 private val bottomNavItems = listOf(
     BottomNavItem(
         screen = Screen.MainSection.Home,
-        selectedIcon = Icons.Filled.MusicNote,
-        unselectedIcon = Icons.Outlined.MusicNote
+        selectedIcon = Icons.Filled.Search,
+        unselectedIcon = Icons.Outlined.Search
     ),
     BottomNavItem(
         screen = Screen.MainSection.Search,
-        selectedIcon = Icons.Filled.Search,
-        unselectedIcon = Icons.Outlined.Search
+        selectedIcon = Icons.Filled.Explore,
+        unselectedIcon = Icons.Outlined.Explore
     ),
     BottomNavItem(
         screen = Screen.MainSection.Favourite,
