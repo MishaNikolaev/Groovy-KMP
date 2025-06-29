f8f7795c8d446f83462e99c3d2aeed501638f80a
Mikhail Nikolaev <nmichail154@mail.ru>
2025-06-29 17:33:55 +0700

    Remove font from ios, i'll fix it later

--- shared/src/iosMain/kotlin/com/nmichail/groovy_kmp/presentation/AlbumFontFamily.ios.kt
+++ shared/src/iosMain/kotlin/com/nmichail/groovy_kmp/presentation/AlbumFontFamily.ios.kt
@@ -1,12 +1,5 @@
 package com.nmichail.groovy_kmp.presentation
 
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 
-actual val AlbumFontFamily: FontFamily = FontFamily(
-    Font(
-        name = "YSMusic_HeadlineBold",
-        weight = FontWeight.Normal
-    )
-) 
+actual val AlbumFontFamily: FontFamily = FontFamily.Default 
