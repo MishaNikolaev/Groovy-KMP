7450ad403f9407de8cc59709474d9e44a09cf1a9
Mikhail Nikolaev <nmichail154@mail.ru>
2025-09-03 21:39:56 +0700

    improvement: delete comments from UiState.kt

--- feature/core/src/commonMain/kotlin/com/nmichail/groovy_kmp/feature/core/state/UiState.kt
+++ feature/core/src/commonMain/kotlin/com/nmichail/groovy_kmp/feature/core/state/UiState.kt
@@ -1,8 +1,5 @@
 package com.nmichail.groovy_kmp.feature.core.state
 
-/**
- * Base UI state interface
- */
 sealed class UiState<out T> {
     object Loading : UiState<Nothing>()
     data class Success<T>(val data: T) : UiState<T>()
@@ -10,45 +7,24 @@ sealed class UiState<out T> {
     object Empty : UiState<Nothing>()
 }
 
-/**
- * Extension function to check if state is loading
- */
 fun <T> UiState<T>.isLoading(): Boolean = this is UiState.Loading
 
-/**
- * Extension function to check if state is success
- */
 fun <T> UiState<T>.isSuccess(): Boolean = this is UiState.Success
 
-/**
- * Extension function to check if state is error
- */
 fun <T> UiState<T>.isError(): Boolean = this is UiState.Error
 
-/**
- * Extension function to check if state is empty
- */
 fun <T> UiState<T>.isEmpty(): Boolean = this is UiState.Empty
 
-/**
- * Extension function to get data from success state
- */
 fun <T> UiState<T>.getDataOrNull(): T? = when (this) {
     is UiState.Success -> data
     else -> null
 }
 
-/**
- * Extension function to get error message from error state
- */
 fun <T> UiState<T>.getErrorMessageOrNull(): String? = when (this) {
     is UiState.Error -> message
     else -> null
 }
 
-/**
- * Extension function to map success data
- */
 fun <T, R> UiState<T>.map(transform: (T) -> R): UiState<R> = when (this) {
     is UiState.Loading -> UiState.Loading
     is UiState.Success -> UiState.Success(transform(data))
